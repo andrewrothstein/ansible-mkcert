346e7f99b3d3c9f29a82a5f74baab1676496b881
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-05-15 22:28:06 -0400

    mkcert:1.4.4

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -20,7 +20,7 @@ dl() {
 
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
@@ -32,10 +32,12 @@ dl_ver() {
     printf "  %s:\n" $ver
 
     dl $ver darwin amd64
+    dl $ver darwin arm64
     dl $ver linux amd64
     dl $ver linux arm
     dl $ver linux arm64
     dl $ver windows amd64 .exe
+    dl $ver windows arm64 .exe
 }
 
-dl_ver ${1:-v1.4.3}
+dl_ver ${1:-v1.4.4}
